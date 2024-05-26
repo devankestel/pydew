e550f9b9470d921c6ffdc89151a6dede100eb81c
Devan Kestel <devan.kestel@gmail.com>
2024-05-26 13:55:22 -0700

    feat: separate horizontal and vertical movement for dealing with collision later on.

--- player.py
+++ player.py
@@ -45,8 +45,12 @@ class Player(pygame.sprite.Sprite):
         # AKA 1.4X -> 1X
         if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
+
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
 
     def update(self, dt): 
 
