233965408494c59053dea1b1ce7f4201634969c0
Devan Kestel <devan.kestel@gmail.com>
2024-05-26 12:48:41 -0700

    feat: directions.

--- player.py
+++ player.py
@@ -13,3 +13,31 @@ class Player(pygame.sprite.Sprite):
         )
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
+
+        self.direction = pygame.math.Vector2()
+
+    def input(self):
+
+        keys = pygame.key.get_pressed()
+
+        # vertical axis
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else: 
+            self.direction.y = 0
+        
+        # horizontal axis
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else: 
+            self.direction.x = 0
+
+        print(self.direction)
+
+    def update(self, dt): 
+
+        self.input()
